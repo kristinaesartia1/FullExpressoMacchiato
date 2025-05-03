5541c912608edc915df200c0965e813b35b3f910
Alessio <alessio.velluso@gmail.com>
2025-05-03 12:34:59 +0200

    :sparkles: swagger-ui link on home page

--- FRONT/src/pages/Welcomer.vue
+++ FRONT/src/pages/Welcomer.vue
@@ -5,12 +5,20 @@ import { useRouter } from 'vue-router';
 
 const router = useRouter();
 
+const navigateToSwagger = () =>
+{
+    const devSwaggerUrl = 'http://127.0.0.1:3000/swagger-ui';
+    window.open(devSwaggerUrl, '_blank')
+}
 </script>
 
 <template>
     <Container padding="1rem" style="column-gap: 2rem;">
-        <EmptyMessage class="pointer" theme-color="surface" borders @click="router.push('personalPage')">
+        <EmptyMessage class="pointer mb-2" theme-color="surface" borders @click="router.push('personalPage')">
             Navigate to your personal page to set your preferences
+        </EmptyMessage>
+        <EmptyMessage v-if="import.meta.env.DEV" class="pointer" theme-color="info" borders @click="navigateToSwagger()">
+            SWAGGER-UI
         </EmptyMessage>
     </Container>
 </template>
